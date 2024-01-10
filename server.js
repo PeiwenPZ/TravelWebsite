9ea64e0c5a6a0f85c38477443851466c6d8658e1
PeiwenPZ <imminna@163.com>
2024-01-10 00:56:38 -0800

    connect MongoDB database

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const dotenv= require('dotenv');
+const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+
+const app = require('./app');
 
 //4) start a server
 //creating a port and start listening to port
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}...`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
