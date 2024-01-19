01cfb6d8ea8562fd79aead85350a7f85944de418
PeiwenPZ <imminna@163.com>
2024-01-19 01:50:08 -0800

    add error handling middleware

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
 //1)middlewares
 
 console.log(process.env.NODE_ENV);
@@ -20,24 +21,27 @@ app.use(express.json());
 //middleware for open static files
 app.use(express.static(`${__dirname}/public`));
 
-//create our own middleware
-app.use((req,res,next) => {
-    console.log('Hello from the middleware.');
-    next();
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
-
-app.use((req,res,next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-});
-
 
 //3)use our build in router- mounting the Routes
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
+//handling unhandle Routes
+app.all('*', (req, res, next ) => {
+//   console.log('Handling 404 error');
+//   console.log('Original URL:', req.originalUrl);
+//   res.status(404).json({
+//     status: 'fail',
+//     message: `Cannot find ${req.originalUrl} on this server!!`,
+//   });
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!!`, 404));
+});
 
+app.use(globalErrorHandler);
 
 module.exports = app;
-
---
+++ controllers/errorController.js
@@ -0,0 +1,62 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name; //replace regular expression
+  const message = `Duplicate field value: /name: ${value}/. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      staus: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or unknown error: don't leak error details
+    console.log('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+   //console.log(process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    console.log(err.name);
+    if (err.name === 'CastError') error = handleCastErrorDB(error); // debug here: change error.name to err.name
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
+  }
+};
